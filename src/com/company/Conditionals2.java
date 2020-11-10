d2f189cad74a2a7a359a1f37ea6f93290229c634
rdonati <racheldonati@billericak12.com>
2020-11-10 13:03:51 -0500

    second version

--- src/com/company/Conditionals2.java
+++ src/com/company/Conditionals2.java
@@ -12,10 +12,7 @@ public class Conditionals2 {
     }
 
     public static boolean isLater(int month1, int day1, int year1, int month2, int day2, int year2){
-        if (year1>year2){
-            return true;
-        }
-        else if (year1==year2 && month1>month2){
+        if (year1>year2 || year1==year2 && month1>month2){
             return true;
         }
         else{
@@ -66,38 +63,37 @@ public class Conditionals2 {
 
     public static boolean makeBench(int small, int big, int goal){
         return (small+big*5<=goal);
-        //return (small*12+big*60<=goal);
     }
 
     public static void main (String[] args){
-//        System.out.println("2000 " + isLeapYear(2000));
-//        System.out.println("2004 " + isLeapYear(2004));
-//        System.out.println("2003 " + isLeapYear(2003));
-//        System.out.println("2100 " + isLeapYear(2100));
+        System.out.println("2000 " + isLeapYear(2000));
+        System.out.println("2004 " + isLeapYear(2004));
+        System.out.println("2003 " + isLeapYear(2003));
+        System.out.println("2100 " + isLeapYear(2100));
 
-//        System.out.println("1/2/2010 is later than 1/2/2011 " + isLater(1,2,2010, 1,2,2011));
-//        System.out.println("1/2/2011 is later than 1/2/2010 " + isLater(1,2,2011, 1,2,2010));
-//        System.out.println("1/5/2010 is later than 3/2/2010 " + isLater(1,5,2010, 3,2,2010));
-//        System.out.println("3/2/2010 is later than 1/2/2010 " + isLater(3,2,2010, 1,2,2010));
-//        System.out.println("1/3/2010 is later than 1/2/2010 " + isLater(1,3,2010, 1,2,2010));
-//        System.out.println("1/2/2010 is later than 1/3/2010 " + isLater(1,2,2010, 1,3,2010));
-//        System.out.println("1/2/2010 is later than 1/2/2010 " + isLater(1,2,2010, 1,2,2010));
-//        System.out.println("4/8/2010 is later than 1/2/2011 " + isLater(4,8,2010, 1,2,2011));
+        System.out.println("1/2/2010 is later than 1/2/2011 " + isLater(1,2,2010, 1,2,2011));
+        System.out.println("1/2/2011 is later than 1/2/2010 " + isLater(1,2,2011, 1,2,2010));
+        System.out.println("1/5/2010 is later than 3/2/2010 " + isLater(1,5,2010, 3,2,2010));
+        System.out.println("3/2/2010 is later than 1/2/2010 " + isLater(3,2,2010, 1,2,2010));
+        System.out.println("1/3/2010 is later than 1/2/2010 " + isLater(1,3,2010, 1,2,2010));
+        System.out.println("1/2/2010 is later than 1/3/2010 " + isLater(1,2,2010, 1,3,2010));
+        System.out.println("1/2/2010 is later than 1/2/2010 " + isLater(1,2,2010, 1,2,2010));
+        System.out.println("4/8/2010 is later than 1/2/2011 " + isLater(4,8,2010, 1,2,2011));
 
-//        System.out.println("Best match 2 3 4 " + bestMatch(2,3,4));
-//        System.out.println("Best match 4 3 3 " + bestMatch(4,3,3));
-//        System.out.println("Best match 3 8 4 " + bestMatch(3,8,4));
-//        System.out.println("Best match 4 3 4 " + bestMatch(4,3,4));
-//        System.out.println("Best match 2 4 4 " + bestMatch(2,4,4));
-//        System.out.println("Best match 8 8 4 " + bestMatch(8,8,4));
-//        System.out.println("Best match 4 4 4 " + bestMatch(4,4,4));
+        System.out.println("Best match 2 3 4 " + bestMatch(2,3,4));
+        System.out.println("Best match 4 3 3 " + bestMatch(4,3,3));
+        System.out.println("Best match 3 8 4 " + bestMatch(3,8,4));
+        System.out.println("Best match 4 3 4 " + bestMatch(4,3,4));
+        System.out.println("Best match 2 4 4 " + bestMatch(2,4,4));
+        System.out.println("Best match 8 8 4 " + bestMatch(8,8,4));
+        System.out.println("Best match 4 4 4 " + bestMatch(4,4,4));
 
-//        System.out.println("Find Best fit 2 3 6 is " + findBestFit(2,3,6));
-//        System.out.println("Find Best fit 4 3 6 is " + findBestFit(4,3,6));
-//        System.out.println("Find Best fit 3 4 6 is " + findBestFit(3,4,6));
-//        System.out.println("Find Best fit 2 3 1 is " + findBestFit(2,3,1));
-//        System.out.println("Find Best fit 6 3 4 is " + findBestFit(6,3,4));
-//        System.out.println("Find Best fit 3 6 4 is " + findBestFit(3,6,4));
+        System.out.println("Find Best fit 2 3 6 is " + findBestFit(2,3,6));
+        System.out.println("Find Best fit 4 3 6 is " + findBestFit(4,3,6));
+        System.out.println("Find Best fit 3 4 6 is " + findBestFit(3,4,6));
+        System.out.println("Find Best fit 2 3 1 is " + findBestFit(2,3,1));
+        System.out.println("Find Best fit 6 3 4 is " + findBestFit(6,3,4));
+        System.out.println("Find Best fit 3 6 4 is " + findBestFit(3,6,4));
 
         System.out.println("Make bench 3 1 8 is " + makeBench(3,1,8));
         System.out.println("Make bench 3 1 9 is " + makeBench(3,1,9));
